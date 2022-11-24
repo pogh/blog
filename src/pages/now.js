89de5ca2db2a08ddcb62b2bdb7ddd4fe97fc4348
Peter <pogh@users.noreply.github.com>
2022-11-24 13:04:16 +0100

    Update 24.11.2022

--- src/pages/now.js
+++ src/pages/now.js
@@ -13,20 +13,20 @@ const Now = ({ data, location }) => {
 			<div>
 				<p>
 					This is my now page inspired by <a href="https://sive.rs/now">Derek Sivers</a>,
-					answering the question, “What am I doing now?”  I also like how this question can
-					also be rhetorical.  
+					answering the question, “What am I doing now?” 
 				</p>
-				<header>
-					<p>
-						Updated 24.11.2022
-					</p>
-				</header>
+				<p>
+					I also like how this question can be rhetorical!
+				</p>
+				<p>
+					Updated 24.11.2022
+				</p>
 			</div>
 			<div>
 				<h2>This blog!</h2>
 				<p>
-					I’m still tinkering with Gatsby <a href="https://www.gatsbyjs.com/starters/gatsbyjs/gatsby-starter-blog">gatsby-starter-blog</a>.
-					
+					I’m still tinkering with the Gatsby <a href="https://www.gatsbyjs.com/starters/gatsbyjs/gatsby-starter-blog">gatsby-starter-blog</a>
+					for this site.  
 				</p>
 			</div>
 			<div>
@@ -36,20 +36,22 @@ const Now = ({ data, location }) => {
 				</p>
 			</div>
 			<div>
-				<h2></h2>
-				<p>
-				</p>
-			</div>
-			<div>
 				<h2>GrapheneOS</h2>
 				<p>
 					Continuing my privacy focussed pivot away from the Apple world.
 				</p>
 			</div>
 			<div>
+				<h2>Statistics - An Introduction Using R</h2>
+				<p>
+					Finally got to it and enjoying it.  Take a look at <Link to="/blog-two">blog-two</Link>.
+				</p>
+			</div>
+			<div>
 				<h2>Low Oxalate Diet</h2>
 				<p>
-					The adjustment has been a bit of shock and I’m still working through it, but I wouldn’t have believed the difference.
+					The adjustment has been a bit of shock and I’m still working through it, but I wouldn’t have believed the difference in the way I feel.
+					This is really long term project.
 				</p>
 			</div>
             <footer>
