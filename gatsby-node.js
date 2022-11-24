1a4b7c5e593ec1247269dcf28a76b18a5fc4277a
Peter <pogh@users.noreply.github.com>
2022-11-24 09:18:54 +0100

    Bug Fix: Navigation arrows on blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,38 +48,45 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       var previousPostId
       var nextPostId
 
+      var thisPost
+      var path
+      var indexOfFirst
+
       // Get the root folder of the current node
-      var path = post.fields.slug;
-      var indexOfFirst = path.indexOf('/', 1);
-      var thisPathRoot = path.substring(1, indexOfFirst);
+      thisPost = post
+      path = thisPost.fields.slug
+      indexOfFirst = path.indexOf('/', 1);
+      var currentPathRoot = path.substring(1, indexOfFirst)
 
       // If the previous post is in the same root folder link to it
       if (posts[index - 1] != null) {
 
-        path = posts[index - 1].fields.slug;
-        indexOfFirst = path.indexOf('/', 1);
-        var previousPathRoot = path.substring(1, indexOfFirst);
+        thisPost = posts[index - 1]
+        path = thisPost.fields.slug
+        indexOfFirst = path.indexOf('/', 1)
+        var previousPathRoot = path.substring(1, indexOfFirst)
 
-        if (thisPathRoot != previousPathRoot) {
-          previousPostId = null
+        if (currentPathRoot == previousPathRoot) {
+          previousPostId = index === 0 ? null : thisPost.id
         }
         else {
-          previousPostId = index === 0 ? null : posts[index - 1].id
+          previousPostId = null
         }
       }
 
       // If the next post is in the same root folder link to it
       if (posts[index + 1] != null) {
 
-        path = posts[index + 1].fields.slug;
-        indexOfFirst = path.indexOf('/', 1);
-        var nextPathRoot = path.substring(1, indexOfFirst);
+        thisPost = posts[index + 1]
+        path = thisPost.fields.slug
+        indexOfFirst = path.indexOf('/', 1)
+        var nextPathRoot = path.substring(1, indexOfFirst)
 
-        if (thisPathRoot != nextPathRoot) {
-          nextPostId = null
+        if (currentPathRoot == nextPathRoot) {
+          nextPostId = index === posts.length - 1 ? null : thisPost.id
         }
         else {
-          nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+          nextPostId = null
         }
       }
 
