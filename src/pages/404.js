997333459e4fb74d50f7dbcd552df37cbb80a942
pogh <>
2022-11-27 14:31:12 +0100

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -8,9 +9,11 @@ const NotFoundPage = ({ data, location }) => {
     const siteTitle = data.site.siteMetadata.title
 
     return (
-        <Layout location={location} title={siteTitle}>
-            <h1>404: Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Layout>
+            <p>404: Not Found – Sorry about that.</p>
+            <footer>
+                <Bio />
+            </footer>
         </Layout>
     )
 }
