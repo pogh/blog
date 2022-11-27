f6ccca18ba7208a3f4197d62b50d668cfbf7c6d4
pogh <>
2022-11-27 12:33:12 +0100

    Added plug-in gatsby-remark-katex (LaTex support)

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-katex`,
         ],
       },
     },
